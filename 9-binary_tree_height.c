f747fe5f8378bf1217d2e7f77789aabad3f6dd22
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-02-01 15:23:50 -0800

    Correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,14 +11,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t l = 0, r = 0;
 
 	if (tree == NULL)
+	{
 		return (0);
-
-	/* recursively calculate the height of left and right */
-	l = binary_tree_height(tree->left);
-	r = binary_tree_height(tree->right);
-
-	if (l > r)
-		return (l + 1);
+	}
 	else
-		return (r + 1);
+	{
+		l = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+		r = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	}
+	return ((l > r) ? l : r);
 }
